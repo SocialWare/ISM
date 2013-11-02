b2dd672d3282877e8cdb34c9fb077c06f5649206
Montse Lozano Dieck <montse_967@hotmail.com>
2013-11-02 16:37:06 -0600

    eliminar participant

--- ISM/src/com/ism/Session.java
+++ ISM/src/com/ism/Session.java
@@ -34,7 +34,7 @@ public class Session {
     private String sSessionName;
     private String sTriggering;
     private boolean bHasTriggering;
-    private LinkedList<Participante> lParticipants;
+    private LinkedList<Participant> lParticipants;
     private boolean bHasParticipants;
     private LinkedList<Element> lElements;
     private boolean bHasElements;
