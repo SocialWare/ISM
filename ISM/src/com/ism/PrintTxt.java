a6c6232d786e90caaf9f870f644eb264721cdd68
montselozanod <montselozanod@users.noreply.github.com>
2013-11-19 22:26:52 -0600

    llamar a instancia de generatefile

--- ISM/src/com/ism/PrintTxt.java
+++ ISM/src/com/ism/PrintTxt.java
@@ -31,28 +31,71 @@ import java.awt.print.*;
 import java.awt.Font;
 import static java.awt.print.Printable.NO_SUCH_PAGE;
 import static java.awt.print.Printable.PAGE_EXISTS;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import javax.print.Doc;
+import javax.print.DocFlavor;
+import javax.print.DocPrintJob;
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
+import javax.print.SimpleDoc;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.event.PrintJobAdapter;
+import javax.print.event.PrintJobEvent;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
+ *Clase printTXT que es implementada cuando se le da click al boton print en el main de la aplicacion
  * @author SocialWare
  */
 public class PrintTxt implements Printable, ActionListener{
-    public void actionPerformed(ActionEvent e) {
+    
+        private LinkedList<Element> lista;
+        String fileNameIdeas = "ideas.txt";
+       
+    
+	public void actionPerformed(ActionEvent e) {
         System.out.println("actionperformred");
+        //aqui se crea el printer job 
         PrinterJob job = PrinterJob.getPrinterJob();
         job.setPrintable(this);
-        boolean ok = job.printDialog();
+        boolean ok = job.printDialog(); // aqui se escoge que impresora (dialog box de impresion)
         if (ok) {
-            try {
+            try { //job llama al metodo print
                 job.print();
             } catch (PrinterException ex)
             { /* The job did not successfully complete */ }
         }
     }
+        
+      /*
+       * Indicar la lista de elementos que se desea imprimir
+       */
+      public void setListaElementos(LinkedList<Element> lista){
+          this.lista = lista;
+      }
     public int print(Graphics g, PageFormat pf, int page)
       throws PrinterException {
         // We have only one page, and 'page'
         // is zero-based
+        
+   
+        
+        GenerateFile f = new GenerateFile();
+            try {
+                f.generaArchivoIdeas(lista);
+            } catch (IOException ex) {
+                Logger.getLogger(PrintTxt.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            
+         
+        
         if (page > 0) {
              return NO_SUCH_PAGE;
         }
